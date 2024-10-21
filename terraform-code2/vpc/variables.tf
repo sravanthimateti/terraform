61950d611907178b6c906ca50d09ff9215be4c4c
sravanthimateti <73110097+sravanthimateti@users.noreply.github.com>
2024-10-21 11:21:45 +0530

    Create variables.tf

---
+++ terraform-code2/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  description = "The AWS region to deploy resources in"
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "The CIDR block for the subnet"
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "The availability zone for the subnet"
+  default     = "us-east-1a"
+}
