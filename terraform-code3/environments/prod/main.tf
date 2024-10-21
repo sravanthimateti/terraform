14ea1343921b4c1dda0056078b0f0d4dd9bd5be6
sravanthimateti <73110097+sravanthimateti@users.noreply.github.com>
2024-10-21 11:36:48 +0530

    Create main.tf

---
+++ terraform-code3/environments/prod/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+module "example_ec2_instance" {
+  source              = "../../modules/ec2"
+  ami                 = "ami-0e86e20dae9224db8"
+  instance_type       = "t2.micro"
+  environment         = "prod"
+  availability_zone   = "us-east-1b"
+}
