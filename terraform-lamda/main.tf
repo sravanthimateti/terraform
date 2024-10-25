0fdfd93638c34092dd6b5feab250221a98ce3d9b
sravanthimateti <73110097+sravanthimateti@users.noreply.github.com>
2024-10-25 23:48:28 +0530

    Create main.tf

---
+++ terraform-lamda/main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = var.region
+}
+
+# IAM role for Lambda execution
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach a policy to allow Lambda to log to CloudWatch
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Lambda function
+resource "aws_lambda_function" "my_lambda" {
+  function_name = var.lambda_function_name
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = var.lambda_handler
+  runtime       = var.lambda_runtime
+  filename      = var.lambda_package
+  
+  source_code_hash = filebase64sha256(var.lambda_package)
+  
+  environment {
+    variables = var.lambda_env_vars
+  }
+}
+
+# CloudWatch log group for Lambda
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name              = "/aws/lambda/${aws_lambda_function.my_lambda.function_name}"
+  retention_in_days = var.log_retention
+}
